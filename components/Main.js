15b2dfc00f6ffd6cf9b5b4639ffacae941e67f5b
alex <aorfevre@gmail.com>
2023-11-19 12:22:10 +0100

    wip

--- components/Main.js
+++ components/Main.js
@@ -2,15 +2,9 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import Loading from './Loading/Loading'
 import styles from '../styles/Main.module.css'
-import { Web3Button } from '@web3modal/react'
-import {
-  useAccount,
-  useNetwork,
-  useSwitchNetwork,
-} from 'wagmi'
+
 import { useRouter } from 'next/router'
-import { sendTransaction, waitForTransaction,switchNetwork } from '@wagmi/core'
-import { parseEther } from 'viem'
+
 
 
 
@@ -20,163 +14,12 @@ function Main({ }) {
   const {push} = useRouter()
   const {hash} = router.query
 
-  const [params, setParams] = useState('')
-
-  // Component Wagmi state to avoid hydration warning/error
-  const [connectionStat, setConnectionStat] = useState()
-
-  const [loading, setLoading] = useState(false)
-
-
-  // Wagmi Account
-  const { isConnected } = useAccount()
-  const { chain } = useNetwork()
-  // const { switchNetwork } = useSwitchNetwork()
-
-
-
-  const callDecode = async (params) => {
-    try {
-      const res = await fetch(`${process.env.PUBLIC_API_URL}/decode?hash=${encodeURIComponent(decodeURIComponent(params))}`);
-      const data = await res.json();
-     return data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  //save the transaction hash to my api /play 
-  const savePlayTransaction = async(hash,txhash)=>{
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        txhash,
-        hash
-      })
-    };
-
-    return await fetch(`${process.env.PUBLIC_API_URL}/play`, requestOptions)
-
-  }
-
-  
-  // copy the value to state here
-  useEffect(() => {
-    setConnectionStat(isConnected)
-
-    if(isConnected && hash && chain && chain?.id === Number(process.env.PUBLIC_CHAIN_ID)){
-
-      setTimeout(async() => {  
-
-          console.log
-          const decoded = await callDecode(hash);
-          console.log('Decoded',decoded)
-          if(decoded && decoded.price && decoded.price >0 ){
-            setParams(decoded);
-            handleTransaction(decoded,hash)
-          }
-        
-
-      },1000)
-    }
-
-
-  }, [isConnected,hash])
-
-  useEffect(() => {
-    if (isConnected && chain && chain?.id !== Number(process.env.PUBLIC_CHAIN_ID)) {
-      setTimeout(()=>{
-        switchNetwork({chainId:Number(process.env.PUBLIC_CHAIN_ID)})
-
-      },500)
-    }
-  }, [chain])
-
-
-  const handleTransaction = async(decoded,d) => {
-    setLoading(true)
-    console.log('Doing a transaction',decoded)
-    if(decoded && decoded.price >0){
-      const { hash } = await sendTransaction({
-        chainId: Number(process.env.PUBLIC_CHAIN_ID),
-        to: decoded.payout_wallet,
-        value: parseEther(((decoded.price * decoded.number * 1000)/1000 ).toString()),
-      })
-      console.log('HASH OF THE TRANSCATION',hash)
-      // Transaction is sent ! 
-      savePlayTransaction(d,hash)
-  
-      const data = await waitForTransaction({
-        hash
-      })
-      console.log('data finished',data)
-      if(data){
-        push('/success')
-
-      }
-    }
-
-    setLoading(false)
-  }
 
   return (
-    <main className={styles.main_container}>
-      <div className={styles.container}>
-        <div className={styles.image_container}>
-          <Image
-            priority={true}
-            className={styles.image}
-            src="/presentation.jpg"
-            layout="fill"
-            objectFit="contain"
-            alt="Chefleo"
-          />
-        </div>
+    <main >
+     
 
-        <section className={styles.container_form}>
-          {isConnected && (
-            <>
-               
-               {params && params.price && !isNaN(params.price)? <>
-               <ul>
-                <li>Game: {params.game}</li>
-                <li>Action: {params.action}</li>
-                <li>Bet size per play: {params.price}</li>
-                <li>Number of plays: {params.number}</li>
-                <li>Total bet: {params.number*params.price}</li>
-               </ul>
-               </>: <></>}
-                {params && params.price && !isNaN(params.price)? <>
-                
-                      <button
-                        className={styles.btn}
-                        disabled={loading === true ? 'disabled' : ''}
-                        type="button"
-                        onClick={handleTransaction}
-                      >
-                        {loading ? (
-                          <Loading text={'Buying actions..'} />
-                        ) :  (
-                          `Participate with ${params.number*params.price}ETH`
-                        ) }
-                    </button>
-                  </>: <></>}
-            </>
-          )}
-          <Web3Button icon="hide" label="Connect Wallet" balance="hide" />
-        </section>
 
-        <div className={styles.svg_container}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-            className="h-[180px] w-full fill-current text-[#E5BA73]"
-          >
-            <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z"></path>
-          </svg>
-        </div>
-      </div>
     </main>
   )
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,8 @@
 import "../styles/globals.css";
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { baseGoerli, base } from "wagmi/chains";
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { base,baseGoerli } from 'viem/chains'
 import "bootstrap/dist/css/bootstrap.css";
 
 if (!process.env.WALLET_CONNECT_PROJECT_ID) {
@@ -23,24 +19,44 @@ const chainSelected = findChain(process.env.PUBLIC_CHAIN_ID);
 const chains = [chainSelected];
 const projectId = process.env.WALLET_CONNECT_PROJECT_ID;
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
-  publicClient,
-});
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata
+ })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, defaultChain:baseGoerli,chains,metadata, themeVariables:{
+  "--w3m-background-color": "#FFFF09",
+  "--w3m-accent-color": "#FFFF09",
+  "--w3m-button-border-radius": "50px",
+  "--w3m-background-border-radius": "50px",
+  "--w3m-container-border-radius": "50px",
+  "--w3m-accent-fill-color": "#0C0C0C",
+  "--w3m-color-fg-1": "#ffffff",
+  "--w3m-text-medium-regular-text-transform": "uppercase",
+  "--w3m-text-medium-regular-weight": "600",
+  "--w3m-background-border-radius": "35px",
+  "--w3m-container-border-radius": "35px",
+} })
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <WagmiConfig config={wagmiConfig}>
+        
+
         <Component {...pageProps} />
       </WagmiConfig>
 
-      <Web3Modal
-        projectId={projectId}
-        ethereumClient={ethereumClient}
+      {/* <Web3Modal
+        config={wagmiConfig}
         themeVariables={{
           "--w3m-background-color": "#FFFF09",
           "--w3m-accent-color": "#FFFF09",
@@ -55,7 +71,8 @@ function MyApp({ Component, pageProps }) {
           "--w3m-container-border-radius": "35px",
         }}
         defaultChain={chainSelected}
-      />
+        metadata={metadata}
+      /> */}
     </>
   );
 }
